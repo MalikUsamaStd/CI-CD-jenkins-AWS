d87f92212fac9e753b6dbe55c08d31134c912533
MalikUsamaStd <malikusamastd@gmail.com>
2026-05-18 12:19:32 +0500

    First

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            My name is usama
+            My name is usama Aamir
           </li>
         </ol>
 
